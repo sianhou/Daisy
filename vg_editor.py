c1010121ae184340bf817ec54c351b2a189b9953
sian hou <sianhou1987@gmail.com>
2024-05-25 09:54:57 +0800

    right_click_add_node

--- vg_editor.py
+++ vg_editor.py
@@ -1,6 +1,7 @@
 # coding:utf-8
-
+from PySide6.QtGui import QMouseEvent
 from PySide6.QtWidgets import QWidget, QVBoxLayout
+from PySide6.QtCore import Qt
 
 from vg_node import GraphNode
 from vg_view import VisualGraphicsView
@@ -25,11 +26,9 @@ class VisualGraphEditor(QWidget):
         self.view = VisualGraphicsView(self.scene, self)
         self.layout.addWidget(self.view)
 
-        self.debug_add_node()
-
         self.show()
 
-    def debug_add_node(self):
+    def debug_add_node(self, pos=[0, 0]):
         # port_param1 = ParamPort('width', 'float', '#99ff22')
         # port_param2 = ParamPort('width', 'float', '#99ff22')
         # port_output1 = OutputPort('area', 'float', '#99ff22')
@@ -37,8 +36,7 @@ class VisualGraphEditor(QWidget):
 
         params = []
         params.append(ParamPort('width', 'float', '#99ff22'))
-        params.append(ParamPort('width', 'float', '#99ff22'))
-        params.append(ParamPort('width', 'float', '#99ff22'))
+        params.append(ParamPort('height', 'float', '#99ff22'))
         # params.append(ParamPort('width', 'float', '#99ff22'))
         # params.append(ParamPort('width', 'float', '#99ff22'))
         # params.append(ParamPort('width', 'float', '#99ff22'))
@@ -52,6 +50,15 @@ class VisualGraphEditor(QWidget):
         # outputs.append(OutputPort('area', 'float', '#99ff22'))
 
         # 创建一个port
-        node = GraphNode(title="Area", param_ports=params, output_ports=outputs, is_pure=True)
+        node = GraphNode(title="Area", param_ports=params, output_ports=outputs, is_pure=False)
 
-        self.view.add_graph_node(node, [100, 100])
+        self.view.add_graph_node(node, pos)
+
+    def right_click_add_node(self, mouse_pos):
+        self.debug_add_node([mouse_pos.x(), mouse_pos.y()])
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if event.button() == Qt.RightButton:
+            self.right_click_add_node(self.view.mapToScene(event.pos()))
+        else:
+            super().mousePressEvent(event)
